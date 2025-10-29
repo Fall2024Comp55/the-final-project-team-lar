f6df94c71a4541e75d66b7eb53d859f8c8305877
Angadpaul Deol <a_deol1@u.pacific.edu>
2025-10-29 15:42:44 -0700

    DistractionType added

---
+++ src/DistractionType.java
@@ -0,0 +1,15 @@
+
+public enum DistractionType {
+FLY, FAKE_MONSTER, MOVING_SHADOW, WHISPER, CREAK_SOUND;
+	public String toString() {
+		switch(this) {
+			case FLY: return "fly";
+			case FAKE_MONSTER: return "fake_monster";
+			case MOVING_SHADOW: return "moving_shadow";
+			case WHISPER: return "whispering";
+			case CREAK_SOUND: return "creak_sound";
+			
+		}
+	return "n/a";
+}
+}

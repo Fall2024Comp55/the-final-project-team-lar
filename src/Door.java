7b79abe548a8f8b6bee419c6db4ff8d92489bf04
RyanFlesher <r_flesher@u.pacific.edu>
2025-10-31 16:15:10 -0700

    SLight adjustments to Door

--- src/Door.java
+++ src/Door.java
@@ -4,16 +4,17 @@ public class Door extends GameObject {
 	
 	private String roomID;
 
-public String getID() 
-{
-	return roomID;
-}
-public Door(String roomID)
-{
-	super(0,0,"arrow.png","");
-}
+	public String getID() 
+	{
+		return roomID;
+	}
+	
+	public Door(String roomID)
+	{
+		super(0,0,"arrow.png","");
+	}
 
-public void onMouseAction(MouseEvent e) {
-	getID();
-}
+	public void onMouseAction(MouseEvent e) {
+		this.getID();
+	}
 }

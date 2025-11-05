c01996f7fbe94ce683794d7ffcc38bad921dcd52
Lei'lani Garcia <d_garcia42@u.pacific.edu>
2025-11-05 10:43:34 -0800

    beginning of Room class

---
+++ src/Room.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import acm.graphics.*;
+
+public class Room {
+
+	private int id;
+	private ArrayList<Distraction> distractions;
+	private boolean isVisited;
+	private String imagePath; 
+	private GImage BackgroundImage;
+	//screenDelegate
+	
+	
+	
+	
+	
+}
